cd099aad4f6022c171eae19917593b811e88db08
Bforti <139017911+Bforti@users.noreply.github.com>
2024-01-23 04:30:12 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -1,32 +1,43 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), unique=True)
+    posts = relationship("Post")
+    comments = relationship("Comment")
+    favorites = relationship("Favorite")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    sw_entity_id = Column(Integer, ForeignKey('sw_entity.ID'))
+    user = relationship("User")
+    sw_entity = relationship("SWEntity")
 
-    def to_dict(self):
-        return {}
+class SWEntity(Base):
+    __tablename__ = 'sw_entity'
+    ID = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    type = Column(Enum('planet', 'character', name='sw_entity_type'))
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+# Rest of your schema remains the same...
+
+engine = create_engine('sqlite:///starwars_blog.db')
+Base.metadata.create_all(engine)
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem generating the diagram")
+    raise e
